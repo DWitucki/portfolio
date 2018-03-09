d6feba059a2b73a1ebc79fd4e95bdeb5edd3582d
David Witucki <dcwitucki@gmail.com>
2018-03-08 19:07:04 -0700

    creating construction message

--- src/App.js
+++ src/App.js
@@ -7,11 +7,11 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <img src={logo} className="App-logo" alt="logo" height='200'/>
+          <h1 className="App-title">React App Under Construction</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          The future address of David's dope web app
         </p>
       </div>
     );
